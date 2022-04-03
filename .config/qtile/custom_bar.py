20422a88985524b4e5c814b9e2fd6b89535255b5
Kelly McCuddy <kelly@graewolf.com>
2022-04-03 18:45:29 -0400

    tweaking the bar file

--- .config/qtile/custom_bar.py
+++ .config/qtile/custom_bar.py
@@ -5,7 +5,7 @@ from colors import nord
 
 bar1 = bar.Bar([
     widget.GroupBox(
-        fontsize = 12,
+        fontsize = 14,
         active = nord['pink'],
         inactive = nord['grey'],
         highlight_method = "border",
@@ -15,58 +15,50 @@ bar1 = bar.Bar([
         background = nord['bg']),
     widget.Prompt(),
     widget.WindowName(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['orange'],
         background = nord['bg'],
         padding = 5),
     widget.CurrentLayout(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['yellow'],
         background = nord['bg'],
         padding =5),
     widget.TextBox(
-        fontaize = 12,
+        fontaize = 14,
         text="🕪",
         padding = 6,
         foreground = nord['green'],
         background = nord['bg']),
     widget.Volume(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['green'],
         background = nord['bg'],),
     widget.Clock(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['blue'],
         background = nord['bg'],
         format="%A, %B %d  - "),
     widget.Clock(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['red'],
         background = nord['bg'],
         format="%H:%M"),
-    widget.LaunchBar(progs=[
-        ('⏾', 'systemctl suspend', 'put computer to sleep')],
-        fontsize = 20,
-        foreground = nord['red'],
-        background = nord['bg'],
-        padding = 5,
-        #default_icon='/usr/share/icons/Adwaita/24x24/status/night-light-symbolic.symbolic.png'
-        ),
-    widget.Sep(
-        linewidth = 0,
-        padding = 5,
-        background = nord['bg'],),
+    #widget.Sep(
+    #    linewidth = 0,
+    #    padding = 5,
+    #    background = nord['bg'],),
     
     ],
 
     margin=[10, 10, 5, 10],
     opacity = 1,
-    size = 20
+    size = 25
 )
 
 bar2 = bar.Bar([
     widget.GroupBox(
-        fontsize = 12,
+        fontsize = 14,
         active = nord['pink'],
         inactive = nord['grey'],
         highlight_method = "border",
@@ -76,47 +68,47 @@ bar2 = bar.Bar([
         background = nord['bg']),
     widget.Prompt(),
     widget.WindowName(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['orange'],
         background = nord['bg'],
         padding = 5),
     widget.CurrentLayout(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['yellow'],
         background = nord['bg'],
         padding =5),
     widget.TextBox(
-        fontaize = 12,
+        fontaize = 14,
         text="🕪",
         padding = 6,
         foreground = nord['green'],
         background = nord['bg']),
     widget.Volume(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['green'],
         background = nord['bg'],),
     widget.Clock(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['blue'],
         background = nord['bg'],
         format="%A, %B %d  - "),
     widget.Clock(
-        fontsize = 12,
+        fontsize = 14,
         foreground = nord['red'],
         background = nord['bg'],
         format="%H:%M"),
     widget.LaunchBar(progs=[
         ('⏾', 'systemctl suspend', 'put computer to sleep')],
-        fontsize = 20,
-        foreground = nord['red'],
+        fontsize = 14,
+        foreground = nord['white'],
         background = nord['bg'],
         padding = 5,
         #default_icon='/usr/share/icons/Adwaita/24x24/status/night-light-symbolic.symbolic.png'
         ),
-    widget.Sep(
-        linewidth = 0,
-        padding = 5,
-        background = nord['bg'],),
+    #widget.Sep(
+    #    linewidth = 0,
+    #    padding = 5,
+    #    background = nord['bg'],),
     widget.Systray(
         background = nord['bg'],
         padding = 5),
@@ -125,6 +117,6 @@ bar2 = bar.Bar([
 
     margin=[10, 10, 5, 10],
     opacity = 1,
-    size = 20
+    size = 25
 )
 
