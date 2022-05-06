3b9d360b709f7af4dbcca0697c16377fe89d8e10
GraeWolf <kelly@graewolf.com>
2022-05-06 08:28:31 -0400

    changed to dracula theme

--- .config/qtile/custom_bar.py
+++ .config/qtile/custom_bar.py
@@ -1,53 +1,53 @@
 from libqtile import bar, widget
 
-from colors import nord
+from colors import dracula
 
 
 bar1 = bar.Bar([
     widget.GroupBox(
         fontsize = 14,
-        active = nord['pink'],
-        inactive = nord['grey'],
+        active = dracula['pink'],
+        inactive = dracula['grey'],
         highlight_method = "border",
-        this_current_screen_border = nord['green'],
-        this_screen_border = nord['blue'],
-        foreground = nord['blue'],
-        background = nord['bg']),
+        this_current_screen_border = dracula['green'],
+        this_screen_border = dracula['blue'],
+        foreground = dracula['blue'],
+        background = dracula['bg']),
     widget.Prompt(),
     widget.WindowName(
         fontsize = 14,
-        foreground = nord['orange'],
-        background = nord['bg'],
+        foreground = dracula['orange'],
+        background = dracula['bg'],
         padding = 5),
     widget.CurrentLayout(
         fontsize = 14,
-        foreground = nord['yellow'],
-        background = nord['bg'],
+        foreground = dracula['yellow'],
+        background = dracula['bg'],
         padding =5),
     widget.TextBox(
         fontaize = 14,
         text="🕪",
         padding = 6,
-        foreground = nord['green'],
-        background = nord['bg']),
+        foreground = dracula['green'],
+        background = dracula['bg']),
     widget.Volume(
         fontsize = 14,
-        foreground = nord['green'],
-        background = nord['bg'],),
+        foreground = dracula['green'],
+        background = dracula['bg'],),
     widget.Clock(
         fontsize = 14,
-        foreground = nord['blue'],
-        background = nord['bg'],
+        foreground = dracula['blue'],
+        background = dracula['bg'],
         format="%A, %B %d  - "),
     widget.Clock(
         fontsize = 14,
-        foreground = nord['red'],
-        background = nord['bg'],
+        foreground = dracula['red'],
+        background = dracula['bg'],
         format="%H:%M"),
     #widget.Sep(
     #    linewidth = 0,
     #    padding = 5,
-    #    background = nord['bg'],),
+    #    background = dracula['bg'],),
     
     ],
 
@@ -59,58 +59,58 @@ bar1 = bar.Bar([
 bar2 = bar.Bar([
     widget.GroupBox(
         fontsize = 14,
-        active = nord['pink'],
-        inactive = nord['grey'],
+        active = dracula['pink'],
+        inactive = dracula['grey'],
         highlight_method = "border",
-        this_current_screen_border = nord['green'],
-        this_screen_border = nord['blue'],
-        foreground = nord['blue'],
-        background = nord['bg']),
+        this_current_screen_border = dracula['green'],
+        this_screen_border = dracula['blue'],
+        foreground = dracula['blue'],
+        background = dracula['bg']),
     widget.Prompt(),
     widget.WindowName(
         fontsize = 14,
-        foreground = nord['orange'],
-        background = nord['bg'],
+        foreground = dracula['orange'],
+        background = dracula['bg'],
         padding = 5),
     widget.CurrentLayout(
         fontsize = 14,
-        foreground = nord['yellow'],
-        background = nord['bg'],
+        foreground = dracula['yellow'],
+        background = dracula['bg'],
         padding =5),
     widget.TextBox(
         fontaize = 14,
         text="🕪",
         padding = 6,
-        foreground = nord['green'],
-        background = nord['bg']),
+        foreground = dracula['green'],
+        background = dracula['bg']),
     widget.Volume(
         fontsize = 14,
-        foreground = nord['green'],
-        background = nord['bg'],),
+        foreground = dracula['green'],
+        background = dracula['bg'],),
     widget.Clock(
         fontsize = 14,
-        foreground = nord['blue'],
-        background = nord['bg'],
+        foreground = dracula['blue'],
+        background = dracula['bg'],
         format="%A, %B %d  - "),
     widget.Clock(
         fontsize = 14,
-        foreground = nord['red'],
-        background = nord['bg'],
+        foreground = dracula['red'],
+        background = dracula['bg'],
         format="%H:%M"),
     widget.LaunchBar(progs=[
         ('⏾', 'systemctl suspend', 'put computer to sleep')],
         fontsize = 14,
-        foreground = nord['white'],
-        background = nord['bg'],
+        foreground = dracula['white'],
+        background = dracula['bg'],
         padding = 5,
         #default_icon='/usr/share/icons/Adwaita/24x24/status/night-light-symbolic.symbolic.png'
         ),
     #widget.Sep(
     #    linewidth = 0,
     #    padding = 5,
-    #    background = nord['bg'],),
+    #    background = dracula['bg'],),
     widget.Systray(
-        background = nord['bg'],
+        background = dracula['bg'],
         padding = 5),
     
     ],
