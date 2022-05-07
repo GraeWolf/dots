51c56d383116d73bd19c9d51f6aa3ebd0d763a56
GraeWolf <kelly@graewolf.com>
2022-05-07 17:42:42 -0400

    added 3rd bar

--- .config/qtile/custom_bar.py
+++ .config/qtile/custom_bar.py
@@ -109,9 +109,43 @@ bar2 = bar.Bar([
     #    linewidth = 0,
     #    padding = 5,
     #    background = dracula['bg'],),
-    widget.Systray(
+    #widget.Systray(
+    #    background = dracula['bg'],
+    #    padding = 5),
+    
+    ],
+
+    margin=[10, 10, 5, 10],
+    opacity = 1,
+    size = 25
+)
+
+bar3 = bar.Bar([
+    widget.GroupBox(
+        fontsize = 14,
+        active = dracula['pink'],
+        inactive = dracula['grey'],
+        highlight_method = "border",
+        this_current_screen_border = dracula['green'],
+        this_screen_border = dracula['blue'],
+        foreground = dracula['blue'],
+        background = dracula['bg']),
+    widget.Prompt(),
+    widget.WindowName(
+        fontsize = 14,
+        foreground = dracula['orange'],
         background = dracula['bg'],
         padding = 5),
+    widget.CurrentLayout(
+        fontsize = 14,
+        foreground = dracula['yellow'],
+        background = dracula['bg'],
+        padding =5),
+    widget.Clock(
+        fontsize = 14,
+        foreground = dracula['red'],
+        background = dracula['bg'],
+        format="%H:%M"),
     
     ],
 
