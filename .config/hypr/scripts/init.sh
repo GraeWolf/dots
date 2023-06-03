8318231438ec0bb836e8448128b98978a89b875a
GraeWolf <kmccuddy@protonmail.com>
2023-06-03 17:29:51 -0400

    initial commit

---
+++ .config/hypr/scripts/init.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#picom --config ~/.config/picom.conf &
+#wallpaper &
+#nm-applet &
+#volumeicon &
+#sxhkd &
+#nitrogen --restore &
+rog-control-center &
+megasync &
---
+++ .config/nvim/lua/plugins/dracula2.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "maxmx03/dracula.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
