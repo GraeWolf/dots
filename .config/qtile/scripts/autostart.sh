9cea61f7ab4bd8faf182114cc7122753d024dabf
GraeWolf <kmccuddy@protonmail.com>
2023-10-17 11:18:36 -0400

    added screen lock script

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+~/.config/qtile/scripts/locker.sh &
 picom --config ~/.config/picom.conf &
 ~/bin/wallpaper &
 nm-applet &
 volumeicon &
+~/.screenlayout/qtile_layout.sh &
 sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-megasync &
+#megasync &
+dropbox start &
