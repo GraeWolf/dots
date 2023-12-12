b8cdbe20198bb9cf3369fbdf607f474138f252d7
GraeWolf <kmccuddy@protonmail.com>
2023-12-12 09:08:26 -0500

    changed colorscheme to Catppuccin

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,12 +13,12 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("Victor Mono")
 config.font_size = 14.0
 
 config.enable_tab_bar = false
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.95
 
 -- and finally, return the configuration to wezterm
 return config
